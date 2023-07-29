3c7ad7cdf9b3f88c0806e51bdd1a2928a9759143
beloner <belone00@gmail.com>
2023-07-29 02:17:28 -0300

    added fontawesome, lil bit prettier

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12">
-              <a class="btn btn-secondary" href="{{route('portfolio.index')}}">Portfolio</a>
+    <div class="container">
+        <p class="mt-3 text-primary display-6 ">Hey  {{auth()->user()->name}}! Welcome back</p>
+        <div class="card mt-4 px-2">
+            <div class="card-body ">
+                <div class="row">
+                <a href="{{route('portfolio.index')}}" class=" text-center fs-4 rounded col-2 p-3 border border-primary">
+                    Portfolio
+                </a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <p class="text-primary">asd</p>
+        <p class="mt-5 text-primary display-5 ">Welcome to investment-app </p>
+        <a href="{{ route('login') }}" class="p-3 mt-2 btn btn-primary rounded-pill">Get Started</a>
     </div>
 @endsection
