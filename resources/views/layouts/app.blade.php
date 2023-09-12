f77238e943413561977d54e60c0083ca8bb73725
beloner <belone00@gmail.com>
2023-09-12 11:55:58 -0300

    pequeno teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="../../css/app.css">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     @vite(['resources/sass/app.scss', 'resources/js/app.js',])
 </head>
